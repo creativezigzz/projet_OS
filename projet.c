4f3fb869856c0e554ed0ce575804d6ee67a02673
Lucas Silva <he201892@students.ephec.be>
2020-05-20 14:33:55 +0200

    Update projet.c

--- projet.c
+++ projet.c
@@ -4,11 +4,10 @@
 #include <string.h>
 #include <sys/wait.h>
 
-//ps -e | wc -l
 int main(int argc, char* argv[]) {
 	if (argc < 2) {
 
-		char* ok = "Syntaxe correcte ./projet '-parametres palyndrome'\n";
+		char* ok = "Syntaxe correcte ./projet '[-u] palyndrome'\n";
 		write(1, ok, strlen(ok));
 		exit(0);
 	}
@@ -17,42 +16,29 @@ int main(int argc, char* argv[]) {
 		write(2, "Error1", 6);
 		exit(3);
 	}
-	/*On construit le tableau d'argument que prend ./palin*/
-	char* argo[argc + 1];
-	argo[0] = "palin";
-	for (int i = 1; i < argc; i++) {
-		argo[i] = argv[i];
-		//printf("%s\n",argo[i]); Verification new table
-	}
-	argo[argc] = NULL;
 	switch (fork()) {
 		case -1: /* erreur */
 			write(2, "Error2", 6);
 			exit(-1);
-		case 0: /* FILS: execute la commande ps -e */
+		case 0: /* FILS: execute la commande  palin*/
 				 /* la sortie standard du processus est redirigee sur le pipe*/
 			close(STDOUT_FILENO);
 			dup(fp[1]); /* fp[1] sortie standard du processus */
 			close(fp[1]);
 			close(fp[0]); /* le processus ne lit pas dans le pipe */
-			execvp("./palin", argo);/**/
+			execlp("ps","ps","-e");/**/
 			write(2, "Error3\n", 6);
 			exit(3);
 		default: /*El PADRE: pere execute la commande wc -l */
 			   /* l'entree standard du processus est redirigee sur le pipe */
-			wait(NULL);
 			close(STDIN_FILENO);
 			dup(fp[0]); /* fp[1] sortie standard du processus */
 			close(fp[0]);
 			close(fp[1]);
-			char bonjour[30];
-			int bytes;
-			if ((bytes = read(0, bonjour, 30)) == -1) {
-				printf("Error 404\n");
-				exit(2);
-			}
-			write(1, bonjour, bytes);
-			printf("\n");
+			execlp("wc","wc","-l");
+			printf("Error 404\n");
+			exit(2);
+			
 			return 0;
 	}
 }
